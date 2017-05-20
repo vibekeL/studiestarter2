d6a65b66979f2b7389d3d6b0112f295154cfe407
vibekeL <vanl@itu.dk>
2017-05-20 17:51:50 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Brian is the king!!';
+  title = 'Brian is the king-kong!!';
 }
