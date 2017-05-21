08bc467d8b45f3b9c11333128aba40ea75b777db
vibekeL <vanl@itu.dk>
2017-05-21 17:39:24 +0200

    added email and password

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -9,10 +9,25 @@ import { StudentService } from './student.service';
 })
 
 export class LogInComponent implements OnInit {
+
   students: Student[] = [];
+  myStudent: Student;
   constructor(private studentService: StudentService) { }
   ngOnInit(): void {
     this.studentService.getStudents()
       .then(students => this.students = students.slice(1, 5));
+  }
+
+
+    add(email: string, password: string): void {
+     
+    this.myStudent.email =email.trim();
+    this.myStudent.password=password;
+    if (!email || !password) { return; }
+      this.studentService.create(this.myStudent)
+        .then(student => {
+      this.students.push(student);
+      
+    });
   }
 }
